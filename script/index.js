747b5aee488358a7f163a85fc901209961fb3c26
sokkong <sokkonghoun1@gmail.com>
2024-01-08 23:02:02 +0700

    moving data to script

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
-import { SaladMenu, WrapMenu, mueslisMenu, sidesMenu } from "../data/menu.js";
-import { formatCurrency } from "../data/moneyconvert.js";
+import { SaladMenu, WrapMenu, mueslisMenu, sidesMenu } from "./menu.js";
+import { formatCurrency } from "./moneyconvert.js";
 
 function renderMenu(menu) {
   let menuHTML = "";
